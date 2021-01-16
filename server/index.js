cd4e6ddf9633d1b6d354b0001cf42716310b660f
ryeenii <gustarrini@pm.me>
2021-01-16 12:08:32 -0300

    use self-signed certs (for now)

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
-const exp = require('express')
-const ws = exp();
-let domain = ['127.0.0.1', 80];
-ws.use(exp.static('public'));
-ws.listen(domain[1], domain[0], () => {console.log(`sup, server is up at ${domain[0]}:${domain[1]}`)} );
+import express from 'express';
+import https from 'https';
+import fs from 'fs';
+const ws = express();
+const domain = {
+	ip: '127.0.0.1', 
+	port: 443,
+};
+const opts = {
+	key: fs.readFileSync('./certs/test.the80s.zone.key'),
+	cert: fs.readFileSync('./certs/test.the80s.zone.cert'),
+	requestCert: 'false',
+	requestUnauthorized: 'false'
+}
+const hps = https.createServer(opts, ws); 
+ws.use(express.static('public'));
+hps.listen(domain.port, domain.ip, () => {console.log(`sup, server is up at ${domain.ip}:${domain.port}`)} );
