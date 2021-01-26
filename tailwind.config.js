159a8dd9d3a05420c6c901107a5e2462c785bb4b
ryeenii <gustarrini@pm.me>
2021-01-26 02:16:27 -0300

    work in progress support with tailwind + postcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    purge: {
+    	enabled: true,
+    	content: ['./public/**/*.html'],
+    },
+    theme: {
+        colors: {
+            yellow: "#ffe500",
+            orange: "#f28b27",
+            red: "#c429a9",
+            blue: "#4445ff",
+            green: "#0bf131",
+	    DEFAULT: "#150441", 
+	    purple: "#150441" 
+        },
+        fontFamily: {
+            sans: ['Recoleta', 'sans-serif'],
+            alt: ['Recoleta Alt', 'sans-serif']
+        }
+    }
+}
