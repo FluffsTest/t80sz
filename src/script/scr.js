170660d09037b8f00d8dc9c8b944304bdb7f53bf
ryeenii <gustarrini@pm.me>
2021-02-02 16:20:46 -0300

    Initial Svelte support

---
+++ src/script/scr.js
@@ -0,0 +1,6 @@
+import 'svelte'
+import Bobby from '../svelte-html/bobby.svelte'
+const bby = new Bobby({
+	target: document.body
+})
+export default bby
---
+++ src/svelte-html/bobby.svelte
@@ -0,0 +1,50 @@
+  <script>
+  	import '../css/bobby.pcss';
+  </script>
+  <div id="rainbow-cards">
+    <div id="card">
+      <svg xmlns="http://www.w3.org/2000/svg" id="rectcard" preserveAspectRatio="xMin meet" viewBox="0 0 600 800">
+       <defs>
+         <linearGradient x1="91.5%" y1="81.9%" x2="11.2%" y2="18.6%" id="a">
+           <stop stop-color="#0BF131" offset="0%"/>
+           <stop stop-color="#4445FF" offset="25%"/>
+           <stop stop-color="#C429A9" offset="54.6%"/>
+           <stop stop-color="#F28B27" offset="79.1%"/>
+           <stop stop-color="#FFE500" offset="99.9%"/>
+         </linearGradient>
+       </defs>
+        <rect id="cardbase" width="500" height="700" rx="20" ry="20" transform="translate(45,55)" stroke-linecap="round" stroke="url(#a)" stroke-width="20"></rect>
+      </svg>
+    </div>
+    <div id="bobby">
+      <svg xmlns="http://www.w3.org/2000/svg" id="base" preserveAspectRatio="xMin meet" viewBox="-50 -20 1050 800">
+    <defs/>
+        <defs>
+          <linearGradient id="sa" x1="4%" x2="169.6%" y1="46.9%" y2="62.9%">
+            <stop offset="0%" stop-color="#F88"/>
+            <stop offset="100%" stop-color="#E15959"/>
+          </linearGradient>
+        <linearGradient x1="91.5%" y1="52.7%" x2="11.2%" y2="47.4%" id="c">
+          <stop stop-color="#0BF131" offset="0%"/>
+          <stop stop-color="#4445FF" offset="25%"/>
+          <stop stop-color="#C429A9" offset="54.6%"/>
+          <stop stop-color="#F28B27" offset="86.8%"/>
+          <stop stop-color="#FFE500" offset="99.9%"/>
+        </linearGradient>
+        </defs>
+        <g fill="none" fill-rule="evenodd" transform="translate(160 290)">
+            <circle cx="203.5" cy="32.5" r="22" fill="#FFF"/>
+            <circle cx="458.5" cy="32.5" r="22" fill="#FFF"/>
+            <path stroke="#FFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="5" d="M294.8 120.4c5.9 18.8 17.6 28.2 35 28.2s29.8-9.4 37-28.2"/>
+            <rect width="181" height="45" y="63" fill="url(#sa)" rx="22.5"/>
+            <rect width="181" height="45" x="481" y="63" fill="url(#sa)" rx="22.5"/>
+        </g>
+      <g transform="translate(40 20) " fill="none" fill-rule="evenodd" stroke-linejoin="round">
+        <circle stroke="url(#a)" stroke-width="25" cx="446" cy="357" r="357"/>
+        <g stroke-linecap="round">
+          <path xmlns="http://www.w3.org/2000/svg" d="M68 233c-75 5-88 36-38 90 99 109 509 238 803 127 80-32 83-71 8-115" stroke="url(#c)" stroke-width="30" id="arc"/>
+        </g>
+      </g>
+      </svg>
+    </div>
+  </div>
