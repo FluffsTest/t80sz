d989833b953cdd79fa862ea741f250264df18fc7
ryeenii <gustarrini@pm.me>
2021-02-15 14:45:17 -0300

    replace Debug with Menu

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import 'svelte'
 import Bobby from './svelte-html/bobby.svelte'
-import Debug from './svelte-html/menu.svelte'
+import Menu from './svelte-html/menu.svelte'
 
 let site = new Bobby({
     target: document.body
 })
-site += new Debug({
+site += new Menu({
     target: document.body
 })
 export default site
