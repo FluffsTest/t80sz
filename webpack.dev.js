cc5ed5617e07e9c86afefdee328a11f5778aaf69
ryeenii <gustarrini@pm.me>
2021-02-18 14:30:05 -0300

    ayyy the server is fixed

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,15 @@
+const cssExtract = require('mini-css-extract-plugin');
 const htmlWebpack = require('html-webpack-plugin');
 const path = require('path');
 const defcfg = require('./webpack.config');
 const { merge } = require('webpack-merge');
 module.exports = merge(defcfg, {
 	mode: 'development',
+	entry: "./src/index.js",
 	output: {
 		filename: "t80sz.core.js",
-		path: path.resolve(__dirname, "dev")
+		path: path.resolve(__dirname, "dev"),
+		publicPath: '',
 	},
 	plugins: [
 		new htmlWebpack({
@@ -15,16 +18,29 @@ module.exports = merge(defcfg, {
 			meta: {viewport: 'width=device-width, initial-scale=1'},
 			hash: true
 		}),
-		new htmlWebpack({
-			filename: "debug.html",
-			template: "src/debug/debug.html"
-		})
+		new cssExtract({
+			filename: "src/[name].css"
+		}),
 	],
 	module: {
 		rules: [
 			{
+        		test: /\.(css|pcss)$/i,
+        		exclude: /node_modules/,
+        		use: [cssExtract.loader, "css-loader", "postcss-loader"]
+      		},
+			{
 				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				type: 'asset/resource'
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'src/fonts',
+							publicPath: '../src/fonts'
+						}
+					}
+				]
 			}
 		]
 	}
