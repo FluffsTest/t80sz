ab6e1cc4d7b74dd92ff5a40b643f632569dd17fe
ryeenii <gustarrini@pm.me>
2021-02-27 00:50:59 -0300

    async loading of main menu

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import 'svelte'
 import Bobby from './svelte-html/bobby.svelte'
 import Menu from './svelte-html/menu.svelte'
-
-new Bobby({
-    target: document.body
-})
-new Menu({
-    target: document.body
-})
+document.body.style.cssText = 'opacity: 0; transition: ease-in-out 200ms;'
+async function loadElements() {
+	new Bobby({
+    	target: document.body
+	})
+	new Menu({
+    	target: document.body
+	})
+}
+loadElements()
+.then(() => { 
+	setTimeout(() => { document.body.style.cssText = 'opacity: 1; transition: ease-in-out 200ms;' }, 1000);
+});
