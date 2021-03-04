05419082e58960e380f59d0c72f269ceba92b48a
ryeenii <gustarrini@pm.me>
2021-03-04 15:32:00 -0300

    fixups

--- src/svelte-html/debug.svelte
+++ src/svelte-html/debug.svelte
@@ -1,41 +1,27 @@
 <script lang="typescript">
-	let fetchModules = [
+	import '../debug/debug.pcss';
+	import Base from './debug/base.svelte';
+	// add your file imports here V
+	import Bobby from './bobby.svelte';
+	import Menu from './menu.svelte';
+	import Docs from './docs/docs.svelte';
+	// add your file debug info here V
+	let modules = [
 		{
 			name: Bobby,
-			public: "Bobby Animation Sequence",
-			id: "bobby-animation"
+			public: 'Bobby Animation Sequence',
+			id: 'bobby-anim'
 		},
 		{
 			name: Menu,
-			public: "Website Main Menu",
-			id: "menu"
-		}
-	]
-	import '../debug/debug.pcss';
-	import Bobby from './bobby.svelte';
-	import Menu from './menu.svelte';
-	function changeModule(module) {
-		activeModule = module; 
-	}
-	let activeModule = 'default';
+			public: 'Main Menu Options',
+			id: 'menu-opts'
+		},
+		{
+			name: Docs,
+			public: 'Documentation',
+			id: 'docs-base'
+		},
+	];
 </script>
-<div id="headline">
-	<header id="title">
-		<p class="title-para">Welcome to the</p>
-		<h1 class="title-h1">Debug Menu</h1>	
-	</header>
-	<nav id="module-list">
-		{#each fetchModules as mdl}
-			<div id="mdl" class="{mdl.id}" on:click="{() => changeModule(mdl.name)}"> 
-				<p>{mdl.public}</p>
-			</div>
-		{/each}
-	</nav>
-</div>
-<div id="playarea">
-	{#if activeModule == 'default'}
-		<h1 class="debug-bg">Debug Menu</h1>
-	{:else}
-		<svelte:component this='{activeModule}' />
-	{/if}
-</div>
+<Base fetchModules={modules} type='module'/>
